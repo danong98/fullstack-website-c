d24768d42f288372cd60f0700ffe4e7775862cfd
Daniel Ong <dherro123@gmail.com>
2023-04-09 22:14:34 -0400

    Updated search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Search from "./components/Search";
+import Header from "./components/Header";
+
 const App = () => {
+  const [word, setWord] = useState(""); // useState hook defines initial variable of word. SetWord updates value of word
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target[0].value);
+  };
+
   return (
     <div>
-      <h1>Images Gallery</h1>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 };
